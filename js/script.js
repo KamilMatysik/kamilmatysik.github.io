4833536a93bd14480aac882de264e91b3a57e825
KamilMatysik <KamilMatysik@outlook.com>
2025-07-12 19:44:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-document.getElementById("sideTabArrowsDiv").addEventListener("click", sideTab);
-function sideTab(){
-    const sidebar = document.getElementById('sideBar');
-    const arrows = document.getElementById('sideTabArrowsDiv');
-    document.getElementById("toBlur").style.filter = "blur(2px)";
-    sidebar.classList.toggle("anim");
-    //arrows.classList.toggle("anim");
-}
+
