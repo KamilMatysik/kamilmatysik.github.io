104ebae090e89e52ab12a6949443ac8d725768a3
Kamil <kmatysik1234@gmail.com>
2025-07-13 13:44:53 +0100

    Light/Dark mode implemented

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+document.getElementById("moonIcon").addEventListener("click", darkMode);
+document.getElementById("sunIcon").addEventListener("click", lightMode);
 
+
+function darkMode(){
+    document.body.setAttribute('data-theme', 'dark')
+}
+function lightMode(){
+    document.body.setAttribute('data-theme', 'light')
+}
